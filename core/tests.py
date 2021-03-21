806228c49e5cea89697f413fffe2cdbf630f2dfa
Denis <namorado@TFGcos-MacBook-Pro.local>
2021-03-21 09:28:04 -0300

    corrigindo cluster

--- core/tests.py
+++ core/tests.py
@@ -20,20 +20,20 @@ class TestPokemonTrade(unittest.TestCase):
         #Pokemons próximos no cluster 3, o mais poderoso de acordo com o algorítmo
         lista1_cluster_3 = ['mew','moltres','azelf'] 
         lista2_cluster_3 = ['mesprit','ho-oh','uxie']
-        #Pokemons próximos no cluster 14, o menos poderoso de acordo com o algorítmo
-        lista1_cluster_14 = ['pidgey','unfezant','pikipek']
-        lista2_cluster_14 = ['spearow','staraptor','fletchling']
+        #Pokemons próximos no cluster 10, o menos poderoso de acordo com o algorítmo
+        lista1_cluster_10 = ['pidgey','unfezant','pikipek']
+        lista2_cluster_10 = ['spearow','staraptor','fletchling']
         #Esses pokemons são muito próximos, portanto a resposta deve ser true (troca justa)
         self.assertTrue(verificar_troca(lista1_cluster_3,lista2_cluster_3))
-        self.assertTrue(verificar_troca(lista1_cluster_14,lista2_cluster_14))
+        self.assertTrue(verificar_troca(lista1_cluster_10,lista2_cluster_10))
 
     def test_troca_false(self):
         lista1_cluster_3 = ['mew','moltres','azelf'] 
-        lista1_cluster_14 = ['pidgey','unfezant','pikipek']
-        lista2_cluster_14 = ['spearow','staraptor','fletchling']
-        # Agora estamos testando pokemons opostos, uma troca entre pokemons do cluster 3 com o cluster 14 não pode ser justa
-        self.assertFalse(verificar_troca(lista1_cluster_3,lista1_cluster_14))
-        self.assertFalse(verificar_troca(lista1_cluster_3,lista2_cluster_14))
+        lista1_cluster_10 = ['pidgey','unfezant','pikipek']
+        lista2_cluster_10 = ['spearow','staraptor','fletchling']
+        # Agora estamos testando pokemons opostos, uma troca entre pokemons do cluster 3 com o cluster 10 não pode ser justa
+        self.assertFalse(verificar_troca(lista1_cluster_3,lista1_cluster_10))
+        self.assertFalse(verificar_troca(lista1_cluster_3,lista2_cluster_10))
 
 if __name__ == '__main__':
     unittest.main()
